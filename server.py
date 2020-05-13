c4b5b979e8a04d83add253154c77644d1a04c2bc
rgoluch <rgoluch@iastate.edu>
2020-05-13 01:49:25 -0500

    last water time should be working

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ def water_plant(status: str):
 def get_last_water():
     db = sqlite3.connect(database)
     cursor = db.cursor()
-    cursor.execute('select * from water_log order by date desc limit 1')
+    cursor.execute('select * from water_log order by time desc,date desc limit 1')
     log = cursor.fetchone()
 
     temp = {
