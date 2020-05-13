ae6e7b11bb272dc526efe5f6bdf1744fb0156ccb
rgoluch <rgoluch@iastate.edu>
2020-05-13 01:25:46 -0500

    last water data

--- server.py
+++ server.py
@@ -17,23 +17,41 @@ database = 'plant_data'
 def temp():
     return "hello world"
 
+
 @app.route("/water/<status>", methods=["POST"])
 def water_plant(status: str):
-    print(status)
     water(status)
-    # TODO add in a call to write last water time
-    return "Watering Plant!"
+    if status == 'on':
+        insert = (str(datetime.datetime.now().date()), str(datetime.datetime.now().time()))
+        query = """insert into water_log (date, time) values (?,?)"""
+        with sqlite3.connect(database) as db:
+            cursor = db.cursor()
+            cursor.execute(query,insert)
+            db.commit()
+            db.close()
+    return "command sent!"
+
 
 @app.route("/last_water")
 def get_last_water():
-    # TODO
-    return True
+    db = sqlite3.connect(database)
+    cursor = db.cursor()
+    cursor.execute('select * from water_log order by column desc limit 1')
+    log = cursor.fetchall()
 
+    temp = {
+            "date" : log[0],
+            "time" : log[1]
+        }
+    return jsonify(temp)
+
+# TODO
 @app.route("/auto/<setting>", methods=["PUT"])
 def set_auto_water(setting: str):
     if setting == "on":
         return "Auto water on!"
     return "Auto water off!"
+
 
 @app.route("/data", methods=["GET"])
 def get_sensor_data():
@@ -52,6 +70,7 @@ def get_sensor_data():
         data.append(temp)
     return jsonify(data)
 
+
 @app.route("/add", methods=['GET'])
 def add_sensor_data():
     data = plant_data()
@@ -66,9 +85,11 @@ def add_sensor_data():
         
     return "Inserted data"
 
+
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(add_sensor_data, 'interval', seconds=30)
 sched.start()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
