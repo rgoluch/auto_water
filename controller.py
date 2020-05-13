98d64285eba42d02eb4a51b9951a65d563520ba2
rgoluch <rgoluch@iastate.edu>
2020-05-12 21:19:02 -0500

    adding mongo

--- controller.py
+++ controller.py
@@ -13,12 +13,8 @@ ss = Seesaw(i2c_bus, addr=0x36)
 def water(status):
     if status == "on":
         os.system("uhubctl -l 1-1 -p 4 -a on")
-        # subprocess.call(["sudo"], ["/home/pi/uhubctl/uhubctl"], ["-l 1-1 -p 4 -a on"])
-        # subprocess.run(["sudo /home/pi/uhubctl/uhubctl", "-l 1-1 -p 4 -a on"])
     if status == "off":
         os.system("uhubctl -l 1-1 -p 4 -a off")
-        # subprocess.run(["sudo /home/pi/uhubctl/uhubctl", "-l 1-1 -p 4 -a off"])
-        # subprocess.call(["sudo"], ["/home/pi/uhubctl/uhubctl"], ["-l 1-1 -p 4 -a off"])
 
 
 def auto_water():
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
 from controller import *
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
+client: MongoClient = MongoClient("localhost:270017")
+db = client.data
 
 @app.route("/")
 def temp():
@@ -29,8 +33,23 @@ def set_auto_water(setting: str):
 @app.route("/data", methods=["GET"])
 def get_sensor_data():
     data = plant_data()
+
     return jsonify(data)
 
+def add_sensor_data():
+    data = plant_data()
+    temp = data[0]
+    m = data[1]
+
+    reading = {
+        "temp": temp,
+        "moisture": m
+    }
+    db.data.insert_one(reading)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(add_sensor_data, 'interval', seconds='30')
+sched.start()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
